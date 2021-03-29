6a48d8ab51f7d8097e111fda8403af95c6db0e2d
n-y-kim <nyn2265@gmail.com>
2021-03-29 21:12:58 +0900

    Create standard_modules.py

---
+++ 0329/standard_modules.py
@@ -0,0 +1,64 @@
+import math
+
+# 코사인 함수 (모든 삼각함수는 라디안을 사용합니다)
+print(math.cos(0))
+
+# 로그 함수
+print(math.log10(100))
+
+#
+#
+import random
+
+# 랜덤한 정수 1 <= N <= 20 
+print(random.randint(1, 20))
+
+# 랜덤한 소수 0 <= x <= 1
+print(random.uniform(0, 1))
+
+#
+#
+import datetime 
+
+# 현재 시간과 날짜
+today = datetime.datetime.now()
+print(today)
+
+# 출력값을 "요일, 월 일 연도"로 포매팅
+print(today.strftime("%A, %B %dth %Y"))
+
+# 특정 시간과 날짜
+pi_day = datetime.datetime(2020, 3, 14, 13, 6, 15)
+print(pi_day)
+
+# 두 datetime의 차이
+print(today - pi_day)
+
+#
+#
+import os
+
+# 현재 어떤 계정으로 로그인 돼있는지 확인
+print(os.getlogin())
+
+# 현재 파일의 디렉토리 확인 
+print(os.getcwd())
+
+# 현재 프로세스 ID 확인 
+print(os.getpid())
+
+#
+#
+import os.path
+
+# 프로젝트 디렉토리 경로 '/Users/codeit/PycharmProjects/standard_modules'
+# 현재 파일 경로 '/Users/codeit/PycharmProjects/standard_modules/main.py'
+ 
+# 주어진 경로를 절대 경로로
+print(os.path.abspath('..'))
+
+# 주어진 경로를 현재 디렉토리를 기준으로 한 상대 경로로
+print(os.path.relpath('/Users/codeit/PycharmProjects'))
+
+# 주어진 경로들을 병합
+print(os.path.join('/Users/codeit/PycharmProjects', 'standard_modules'))
